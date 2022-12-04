7203e4f71b53bbbcab942c4069e2a88b5535d7e0
KANG DONGHA <knamuuu@naver.com>
2022-12-04 19:02:37 +0900

    error status fix

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.post("/return", async function (req, res) {
     if (response === 400) {
       res.status(400).end("대여중인 텀블러가 아닙니다.");
     } else if (response === 401) {
-      res.status(401).end("반납 기한이 지났습니다.");
+      res.status(403).end("반납 기한이 지났습니다.");
     } else {
       res.status(200).end("반납처리가 완료되었습니다.");
     }
